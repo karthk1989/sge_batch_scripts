7bc2880f3b1ccc0ae4e651e10e047e81eb26d020
Soph <soph.e.king123@gmail.com>
2017-09-05 03:16:51 +0100

    Re-order dets - put nd280-basket next to SK, so the first 320 bins are SK+ND280 as usual

--- work/1_fvs/covs/make_prodx_cov.cc
+++ work/1_fvs/covs/make_prodx_cov.cc
@@ -6,9 +6,9 @@
 //
 
 
-// ( sk + one-hk + nd2 + nd5 )*2 = 8
+// ( sk + one-hk + nd5 + nd2 )*2 = 8
 // OR 
-// ( sk + one-hk + nd2 + nd9*7 )*2 = 17
+// ( sk + one-hk + nd5 + nd9*7 )*2 = 17
 
 {
 
@@ -18,12 +18,8 @@
   int dets = 0;
 
   bool b_sk_1 = true;		if( b_sk_1 ) dets++;   
-  bool b_hktoch_2 = true;	if( b_hktoch_2 ) dets++;
-  bool b_hkhak_3 = false;	if( b_hkhak_3 ) dets++;
-  bool b_hkkbis_4 = false;	if( b_hkkbis_4 ) dets++;
-  bool b_hktoch_2_5 = false;	if( b_hktoch_2_5 ) dets++;
-  bool b_nd2_fv = false;	if( b_nd2_fv ) dets++;
   bool b_nd5_fv = true;		if( b_nd5_fv ) dets++;
+  bool b_nd2_fv = false;	if( b_nd2_fv ) dets++;
   bool b_nd9_fv1_h10 = true;	if( b_nd9_fv1_h10 ) dets++;
   bool b_nd9_fv2_h10 = true;	if( b_nd9_fv2_h10 ) dets++;
   bool b_nd9_fv3_h10 = true;	if( b_nd9_fv3_h10 ) dets++;
@@ -31,7 +27,10 @@
   bool b_nd9_fv5_h10 = true;	if( b_nd9_fv5_h10 ) dets++;
   bool b_nd9_fv6_h10 = true;	if( b_nd9_fv6_h10 ) dets++;
   bool b_nd9_fv7_h10 = true;	if( b_nd9_fv7_h10 ) dets++; 
-
+  bool b_hktoch_2 = true;	if( b_hktoch_2 ) dets++;
+  bool b_hkhak_3 = false;	if( b_hkhak_3 ) dets++;
+  bool b_hkkbis_4 = false;	if( b_hkkbis_4 ) dets++;
+  bool b_hktoch_5 = false;	if( b_hktoch_5 ) dets++;
   bool b_fhc = true;  bool b_rhc = true;  if(b_fhc && b_rhc) dets=dets*2;
   
   //Set up for the covariance calculation
@@ -46,12 +45,8 @@
 
  		//// fhc ////								//// rhc ////
  std::string  sk_1_fhc= systDir+"1_sk_fhc_" + NAME;			std::string  sk_1_rhc= systDir+"1_sk_rhc_" + NAME;  		
- std::string  hktoch_2_fhc= systDir+"2_hktoch_fhc_" + NAME;		std::string  hktoch_2_rhc= systDir+"2_hktoch_rhc_" + NAME;	
- std::string  hkhak_3_fhc= systDir+"3_hkhak_fhc_" + NAME;		std::string  hkhak_3_rhc= systDir+"3_hkhak_rhc_" + NAME;	
- std::string  hkkbis_4_fhc= systDir+"4_hkkbis_fhc_" + NAME;		std::string  hkkbis_4_rhc= systDir+"4_hkkbis_rhc_" + NAME;	
- std::string  hktoch_5_fhc= systDir+"5_hkkboh_fhc_" + NAME;		std::string  hktoch_5_rhc= systDir+"5_hkkboh_rhc_" + NAME;	
- std::string  nd2_fhc_fv= systDir+"nd2_fhc_fv_" + NAME;			std::string  nd2_rhc_fv= systDir+"nd2_rhc_fv_" + NAME; 		
  std::string  nd5_fhc_fv= systDir+"nd5_fhc_fv_" + NAME;			std::string  nd5_rhc_fv= systDir+"nd5_rhc_fv_" + NAME;		
+ std::string  nd2_fhc_fv= systDir+"nd2_fhc_fv_" + NAME;			std::string  nd2_rhc_fv= systDir+"nd2_rhc_fv_" + NAME; 		
  std::string  nd9_fhc_fv1_h10= systDir+"nd9_fhc_fv1_h10_" + NAME;	std::string  nd9_rhc_fv1_h10= systDir+"nd9_rhc_fv1_h10_" + NAME;
  std::string  nd9_fhc_fv2_h10= systDir+"nd9_fhc_fv2_h10_" + NAME;	std::string  nd9_rhc_fv2_h10= systDir+"nd9_rhc_fv2_h10_" + NAME;
  std::string  nd9_fhc_fv3_h10= systDir+"nd9_fhc_fv3_h10_" + NAME;	std::string  nd9_rhc_fv3_h10= systDir+"nd9_rhc_fv3_h10_" + NAME;
@@ -59,15 +54,15 @@
  std::string  nd9_fhc_fv5_h10= systDir+"nd9_fhc_fv5_h10_" + NAME;	std::string  nd9_rhc_fv5_h10= systDir+"nd9_rhc_fv5_h10_" + NAME;
  std::string  nd9_fhc_fv6_h10= systDir+"nd9_fhc_fv6_h10_" + NAME;	std::string  nd9_rhc_fv6_h10= systDir+"nd9_rhc_fv6_h10_" + NAME;
  std::string  nd9_fhc_fv7_h10= systDir+"nd9_fhc_fv7_h10_" + NAME;	std::string  nd9_rhc_fv7_h10= systDir+"nd9_rhc_fv7_h10_" + NAME;
- 
+ std::string  hktoch_2_fhc= systDir+"2_hktoch_fhc_" + NAME;		std::string  hktoch_2_rhc= systDir+"2_hktoch_rhc_" + NAME;	
+ std::string  hkhak_3_fhc= systDir+"3_hkhak_fhc_" + NAME;		std::string  hkhak_3_rhc= systDir+"3_hkhak_rhc_" + NAME;	
+ std::string  hkkbis_4_fhc= systDir+"4_hkkbis_fhc_" + NAME;		std::string  hkkbis_4_rhc= systDir+"4_hkkbis_rhc_" + NAME;	
+ std::string  hktoch_5_fhc= systDir+"5_hkkboh_fhc_" + NAME;		std::string  hktoch_5_rhc= systDir+"5_hkkboh_rhc_" + NAME;	
+
  
  std::string  N_sk_1_fhc= nomDir+"1_sk_fhc_NOM_.root";			std::string  N_sk_1_rhc= nomDir+"1_sk_rhc_NOM_.root";				
- std::string  N_hktoch_2_fhc= nomDir+"2_hktoch_fhc_NOM_.root";		std::string  N_hktoch_2_rhc= nomDir+"2_hktoch_rhc_NOM_.root";		
- std::string  N_hkhak_3_fhc= nomDir+"3_hkhak_fhc_NOM_.root";		std::string  N_hkhak_3_rhc= nomDir+"3_hkhak_rhc_NOM_.root";		
- std::string  N_hkkbis_4_fhc= nomDir+"4_hkkbis_fhc_NOM_.root";		std::string  N_hkkbis_4_rhc= nomDir+"4_hkkbis_rhc_NOM_.root";		
- std::string  N_hktoch_5_fhc= nomDir+"5_hkkboh_fhc_NOM_.root";		std::string  N_hktoch_5_rhc= nomDir+"5_hkkboh_rhc_NOM_.root";		
- std::string  N_nd2_fhc_fv= nomDir+"nd2_fhc_fv_NOM_.root";		std::string  N_nd2_rhc_fv= nomDir+"nd2_rhc_fv_NOM_.root"; 		
  std::string  N_nd5_fhc_fv= nomDir+"nd5_fhc_fv_NOM_.root";		std::string  N_nd5_rhc_fv= nomDir+"nd5_rhc_fv_NOM_.root";			
+ std::string  N_nd2_fhc_fv= nomDir+"nd2_fhc_fv_NOM_.root";		std::string  N_nd2_rhc_fv= nomDir+"nd2_rhc_fv_NOM_.root"; 		
  std::string  N_nd9_fhc_fv1_h10= nomDir+"nd9_fhc_fv1_h10_NOM_.root";	std::string  N_nd9_rhc_fv1_h10= nomDir+"nd9_rhc_fv1_h10_NOM_.root";	
  std::string  N_nd9_fhc_fv2_h10= nomDir+"nd9_fhc_fv2_h10_NOM_.root";	std::string  N_nd9_rhc_fv2_h10= nomDir+"nd9_rhc_fv2_h10_NOM_.root";	
  std::string  N_nd9_fhc_fv3_h10= nomDir+"nd9_fhc_fv3_h10_NOM_.root";	std::string  N_nd9_rhc_fv3_h10= nomDir+"nd9_rhc_fv3_h10_NOM_.root";	
@@ -75,7 +70,11 @@
  std::string  N_nd9_fhc_fv5_h10= nomDir+"nd9_fhc_fv5_h10_NOM_.root";	std::string  N_nd9_rhc_fv5_h10= nomDir+"nd9_rhc_fv5_h10_NOM_.root";	
  std::string  N_nd9_fhc_fv6_h10= nomDir+"nd9_fhc_fv6_h10_NOM_.root";	std::string  N_nd9_rhc_fv6_h10= nomDir+"nd9_rhc_fv6_h10_NOM_.root";	
  std::string  N_nd9_fhc_fv7_h10= nomDir+"nd9_fhc_fv7_h10_NOM_.root";	std::string  N_nd9_rhc_fv7_h10= nomDir+"nd9_rhc_fv7_h10_NOM_.root";	
-      
+ std::string  N_hktoch_2_fhc= nomDir+"2_hktoch_fhc_NOM_.root";		std::string  N_hktoch_2_rhc= nomDir+"2_hktoch_rhc_NOM_.root";		
+ std::string  N_hkhak_3_fhc= nomDir+"3_hkhak_fhc_NOM_.root";		std::string  N_hkhak_3_rhc= nomDir+"3_hkhak_rhc_NOM_.root";		
+ std::string  N_hkkbis_4_fhc= nomDir+"4_hkkbis_fhc_NOM_.root";		std::string  N_hkkbis_4_rhc= nomDir+"4_hkkbis_rhc_NOM_.root";		
+ std::string  N_hktoch_5_fhc= nomDir+"5_hkkboh_fhc_NOM_.root";		std::string  N_hktoch_5_rhc= nomDir+"5_hkkboh_rhc_NOM_.root";		
+    
 
 
    
@@ -85,18 +84,10 @@
    int it = 0; 
    if( b_sk_1 && b_fhc ) 	{ strcpy( filePrefix[it], sk_1_fhc.c_str() ) ;  	strcpy( nomFile[it], N_sk_1_fhc.c_str() ) ;  it++ ; 		}
    if( b_sk_1 && b_rhc )	{ strcpy( filePrefix[it], sk_1_rhc.c_str() ) ;  	strcpy( nomFile[it], N_sk_1_rhc.c_str() ) ;  it++ ; 		}
-   if( b_hktoch_2 && b_fhc )	{ strcpy( filePrefix[it], hktoch_2_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hktoch_2_fhc.c_str() ) ;  it++ ; 	}
-   if( b_hktoch_2 && b_rhc )	{ strcpy( filePrefix[it], hktoch_2_rhc.c_str() ) ;     	strcpy( nomFile[it], N_hktoch_2_rhc.c_str() ) ;  it++ ;		}
-   if( b_hkhak_3 && b_fhc )	{ strcpy( filePrefix[it], hkhak_3_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hkhak_3_fhc.c_str() ) ;  it++ ; 		}
-   if( b_hkhak_3 && b_rhc )	{ strcpy( filePrefix[it], hkhak_3_rhc.c_str() ) ;      	strcpy( nomFile[it], N_hkhak_3_rhc.c_str() ) ;  it++ ;		}
-   if( b_hkkbis_4 && b_fhc )	{ strcpy( filePrefix[it], hkkbis_4_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hkkbis_4_fhc.c_str() ) ;  it++ ;		}
-   if( b_hkkbis_4 && b_rhc )	{ strcpy( filePrefix[it], hkkbis_4_rhc.c_str() ) ;     	strcpy( nomFile[it], N_hkkbis_4_rhc.c_str() ) ;  it++ ;		}
-   if( b_hktoch_2_5 && b_fhc )	{ strcpy( filePrefix[it], hktoch_5_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hktoch_5_fhc.c_str() ) ;  it++ ;		}
-   if( b_hktoch_2_5 && b_rhc )	{ strcpy( filePrefix[it], hktoch_5_rhc.c_str() ) ;     	strcpy( nomFile[it], N_hktoch_5_rhc.c_str() ) ;  it++ ;		}
-   if( b_nd2_fv && b_fhc )	{ strcpy( filePrefix[it], nd2_fhc_fv.c_str() ) ;  	strcpy( nomFile[it], N_nd2_fhc_fv.c_str() ) ;  it++ ;		}	
-   if( b_nd2_fv && b_rhc )	{ strcpy( filePrefix[it], nd2_rhc_fv.c_str() ) ;       	strcpy( nomFile[it], N_nd2_rhc_fv.c_str() ) ;  it++ ;		}
    if( b_nd5_fv && b_fhc )	{ strcpy( filePrefix[it], nd5_fhc_fv.c_str() ) ;  	strcpy( nomFile[it], N_nd5_fhc_fv.c_str() ) ;  it++ ;		}
    if( b_nd5_fv && b_rhc )	{ strcpy( filePrefix[it], nd5_rhc_fv.c_str() ) ;       	strcpy( nomFile[it], N_nd5_rhc_fv.c_str() ) ;  it++ ;		}	
+   if( b_nd2_fv && b_fhc )	{ strcpy( filePrefix[it], nd2_fhc_fv.c_str() ) ;  	strcpy( nomFile[it], N_nd2_fhc_fv.c_str() ) ;  it++ ;		}	
+   if( b_nd2_fv && b_rhc )	{ strcpy( filePrefix[it], nd2_rhc_fv.c_str() ) ;       	strcpy( nomFile[it], N_nd2_rhc_fv.c_str() ) ;  it++ ;		}
    if( b_nd9_fv1_h10 && b_fhc )	{ strcpy( filePrefix[it], nd9_fhc_fv1_h10.c_str() ) ;  	strcpy( nomFile[it], N_nd9_fhc_fv1_h10.c_str() ) ;  it++ ;	}	
    if( b_nd9_fv1_h10 && b_rhc )	{ strcpy( filePrefix[it], nd9_rhc_fv1_h10.c_str() ) ;  	strcpy( nomFile[it], N_nd9_rhc_fv1_h10.c_str() ) ;  it++ ;	}
    if( b_nd9_fv2_h10 && b_fhc )	{ strcpy( filePrefix[it], nd9_fhc_fv2_h10.c_str() ) ;  	strcpy( nomFile[it], N_nd9_fhc_fv2_h10.c_str() ) ;  it++ ;	}	
@@ -111,7 +102,15 @@
    if( b_nd9_fv6_h10 && b_rhc )	{ strcpy( filePrefix[it], nd9_rhc_fv6_h10.c_str() ) ;  	strcpy( nomFile[it], N_nd9_rhc_fv6_h10.c_str() ) ;  it++ ;	}
    if( b_nd9_fv7_h10 && b_fhc )	{ strcpy( filePrefix[it], nd9_fhc_fv7_h10.c_str() ) ;  	strcpy( nomFile[it], N_nd9_fhc_fv7_h10.c_str() ) ;  it++ ;	}
    if( b_nd9_fv7_h10 && b_rhc )	{ strcpy( filePrefix[it], nd9_rhc_fv7_h10.c_str() ) ;  	strcpy( nomFile[it], N_nd9_rhc_fv7_h10.c_str() ) ;  it++ ;	}
-   
+   if( b_hktoch_2 && b_fhc )	{ strcpy( filePrefix[it], hktoch_2_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hktoch_2_fhc.c_str() ) ;  it++ ; 	}
+   if( b_hktoch_2 && b_rhc )	{ strcpy( filePrefix[it], hktoch_2_rhc.c_str() ) ;     	strcpy( nomFile[it], N_hktoch_2_rhc.c_str() ) ;  it++ ;		}
+   if( b_hkhak_3 && b_fhc )	{ strcpy( filePrefix[it], hkhak_3_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hkhak_3_fhc.c_str() ) ;  it++ ; 		}
+   if( b_hkhak_3 && b_rhc )	{ strcpy( filePrefix[it], hkhak_3_rhc.c_str() ) ;      	strcpy( nomFile[it], N_hkhak_3_rhc.c_str() ) ;  it++ ;		}
+   if( b_hkkbis_4 && b_fhc )	{ strcpy( filePrefix[it], hkkbis_4_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hkkbis_4_fhc.c_str() ) ;  it++ ;		}
+   if( b_hkkbis_4 && b_rhc )	{ strcpy( filePrefix[it], hkkbis_4_rhc.c_str() ) ;     	strcpy( nomFile[it], N_hkkbis_4_rhc.c_str() ) ;  it++ ;		}
+   if( b_hktoch_2_5 && b_fhc )	{ strcpy( filePrefix[it], hktoch_5_fhc.c_str() ) ;  	strcpy( nomFile[it], N_hktoch_5_fhc.c_str() ) ;  it++ ;		}
+   if( b_hktoch_2_5 && b_rhc )	{ strcpy( filePrefix[it], hktoch_5_rhc.c_str() ) ;     	strcpy( nomFile[it], N_hktoch_5_rhc.c_str() ) ;  it++ ;		}
+  
 
         
    const int nflavor = 4;
