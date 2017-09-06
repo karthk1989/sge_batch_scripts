5fd4b6b99ea342741dac9c968780728ea2dcad32
sk1806 <soph.e.king123@gmail.com>
2017-09-06 18:16:55 +0100

    Fix typo

--- work/1_fvs/covs/bins.C
+++ work/1_fvs/covs/bins.C
@@ -17,8 +17,8 @@
 
     for( int i=1; i<31; i++ ){
 
-      std::cout << "const int det_"<<i<<"_lowBin="<< binLow << ";  const int det_"<<i<<"_highBin=" << binLow+79 << ":  "
-      "     numu="<< binLow <<"-"<< binLow+19 <<
+      std::cout << "const int det_"<<i<<"_lowBin="<< binLow << ";  const int det_"<<i<<"_highBin=" << binLow+79 << ";  //"
+      "  numu="<< binLow <<"-"<< binLow+19 <<
       ", anumu="<< binLow +20 <<"-"<< binLow+39 <<
       ", nue="<< binLow +40 <<"-"<< binLow+59 <<
       ", anue="<< binLow +60 <<"-"<< binLow+79 << std::endl;
