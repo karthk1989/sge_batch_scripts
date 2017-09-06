5d991e14b1254e4ca85852628d879ff97e0f07fb
sk1806 <soph.e.king123@gmail.com>
2017-09-06 18:06:16 +0100

    Also spits out int variable definitions for bins

--- work/1_fvs/covs/bins.C
+++ work/1_fvs/covs/bins.C
@@ -11,4 +11,22 @@
     
     binLow = binLow +80;
   }
+
+  std::cout<<""<<std::endl;
+  std::cout<<""<<std::endl;
+
+    for( int i=1; i<31; i++ ){
+
+      std::cout << "const int det_"<<i<<"_lowBin="<< binLow << ";  const int det_"<<i<<"_highBin=" << binLow+79 << ":  "
+      "     numu="<< binLow <<"-"<< binLow+19 <<
+      ", anumu="<< binLow +20 <<"-"<< binLow+39 <<
+      ", nue="<< binLow +40 <<"-"<< binLow+59 <<
+      ", anue="<< binLow +60 <<"-"<< binLow+79 << std::endl;
+
+    binLow = binLow +80;
+  }
+
+
+
+
 }
