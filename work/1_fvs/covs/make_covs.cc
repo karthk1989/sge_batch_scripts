d1927f08bce05bb2c5cdc8637cf6ef468619e409
sk1806 <soph.e.king123@gmail.com>
2018-02-11 21:57:01 +0000

    no change

--- work/1_fvs/covs/make_covs.cc
+++ work/1_fvs/covs/make_covs.cc
@@ -8,7 +8,7 @@
 
 {
 
-  bool alttert		= true;
+  bool alttert		= false;
   bool bmpt 		= false;
   bool harp 		= false;
   bool na61 		= false;		int na61Throws = 100;
@@ -19,7 +19,7 @@
   bool prodx 		= false;		int prodxThrows = 100;
   bool snextbar		= false;
   bool snleadbar	= false;
-  bool snmult		= false;		int snmultThrows = 100;
+  bool snmult		= true;		int snmultThrows = 100;
 
   std::string save_prefix = "1_fvs__sk_nd5_nd2_nd9h7p7_hk2_hk3_hkk4_hhk5_";
 
